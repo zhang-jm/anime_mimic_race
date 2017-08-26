6ecf5bdde5be914742b06685917282a1180c310d
Student <Student@aii.stu>
2017-08-25 23:54:05 -0700

    Added Game Manager Singleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("GameMaster");
+                go.AddComponent<GameManager>();
+            }
+            return _instance;
+        }
+    }
+
+    void Awake() {
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+	}
+}

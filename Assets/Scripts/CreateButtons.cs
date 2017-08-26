d833d278fb916580712b0edd10929d4382ac9558
Jamie Zhang <jmzhang003@gmail.com>
2017-08-26 11:44:17 -0700

    working on trap placement ui

---
+++ Assets/Scripts/CreateButtons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateButtons : MonoBehaviour {
+
+    public GameObject button;
+
+    private List<GameObject> buttons = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < 5; i++)
+        {
+            Debug.Log("asdf");
+            GameObject b = Instantiate(button, Vector3.zero, Quaternion.identity);
+            b.transform.SetParent(transform);
+            b.transform.position = new Vector3((i * 100) + 200, 468, 0);
+            buttons.Add(b);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

38e6bd197c5dcb173cb091419e3d0afd5a5b39eb
Student <Student@aii.stu>
2017-08-26 01:44:40 -0700

    added basic player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Player : MonoBehaviour {
+
+    public float maxVelocity = 10.0f;
+    public float acceleration = 1.5f;
+    public float gravity = 1.0f;
+    public float jumpHeight = 3.0f;
+    public float floatTime = 1.0f;
+
+    private float velocityX = 0;
+    private float velocityY = 0;
+    private float startingHeight = 0;
+    private float timeFloated = 0;
+
+    private bool jumping = false;
 
 	// Use this for initialization
 	void Start () {
@@ -11,6 +24,64 @@ public class NewBehaviourScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+       if(velocityX < maxVelocity) //set max running speed
+        {
+            //v1 = v0 + at
+            velocityX += acceleration * Time.deltaTime;
+        }
+
+        //p1 = p0 + vt
+        transform.position += Vector3.right * velocityX * Time.deltaTime;
+
+        //jump
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!jumping)
+            {
+                jumping = true;
+                velocityY = -5.0f;
+                startingHeight = transform.position.y;
+            }
+        }
+
+        if (jumping)
+        {
+            velocityY += gravity * Time.deltaTime;
+            transform.position -= Vector3.up * velocityY * Time.deltaTime;
+
+            if(transform.position.y < startingHeight)
+            {
+                transform.position = new Vector3(transform.position.x, startingHeight, transform.position.z);
+                jumping = false;
+            }
+        }
+
+        //Debug.Log("Y value: " + transform.position.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("collision detected!");
+
+        if (other.gameObject.tag == "platform")
+        {
+            if (jumping)
+            {
+                jumping = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("trigger detected!");
+
+        if (other.gameObject.tag == "platform")
+        {
+            if (jumping)
+            {
+                jumping = false;
+            }
+        }
+    }
 }

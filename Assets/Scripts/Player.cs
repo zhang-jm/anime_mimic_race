74a3cae3bd92dba72df8e6373986ddbe32476e78
Jamie Zhang <jmzhang003@gmail.com>
2017-08-26 09:29:49 -0700

    add finish line and speed up trap

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,23 +11,29 @@ public class Player : MonoBehaviour {
     public float gravity = 10.0f;
 
     public float trapStopAmtTime = 3.0f;
+    public float trapSpeedAmtTime = 1.0f;
+    public float slowedMaxVelocity = 5.0f;
+    public float speedyMaxVelocity = 20.0f;
 
     public KeyCode jumpKey = KeyCode.Space;
 
     private float velocityX = 0;
     private float velocityY = 0;
     private float startingHeight = 0;
-    private float timeFloated = 0;
     private float statusCounter = 0;
 
     private bool jumping = false;
     private Status status = Status.normal;
 
+    private float raceTime = 0.0f;
+
     public enum Status
     {
         normal,
         slowed,
-        stopped
+        stopped, 
+        speedy,
+        finished
     }
 
 	// Use this for initialization
@@ -37,6 +43,13 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if(status == Status.finished)
+        {
+            return; //do nothing
+        }
+
+        raceTime += Time.deltaTime;
+
         if(status == Status.stopped)
         {
             statusCounter += Time.deltaTime;
@@ -48,11 +61,31 @@ public class Player : MonoBehaviour {
 
             return;
         }
-
-        if(velocityX < maxVelocity) //set max running speed
+        else if (status == Status.speedy)
         {
-            //v1 = v0 + at
-            velocityX += acceleration * Time.deltaTime;
+            statusCounter += Time.deltaTime;
+            if (statusCounter >= trapSpeedAmtTime)
+            {
+                statusCounter = 0;
+                velocityX = maxVelocity;
+                status = Status.normal;
+            }
+        }
+
+        if(status == Status.normal) {
+            if (velocityX < maxVelocity) //set max running speed
+            {
+                //v1 = v0 + at
+                velocityX += acceleration * Time.deltaTime;
+            }
+        }
+        else if (status == Status.slowed)
+        {
+            if (velocityX < slowedMaxVelocity) //set max running speed
+            {
+                //v1 = v0 + at
+                velocityX += acceleration * Time.deltaTime;
+            }
         }
 
         //p1 = p0 + vt
@@ -109,8 +142,31 @@ public class Player : MonoBehaviour {
             }
         } else if (other.gameObject.tag == "stopping_trap")
         {
+            statusCounter = 0;
             transform.position = new Vector3(other.gameObject.transform.position.x, transform.position.y, transform.position.z);
             status = Status.stopped;
+            velocityX = 0;
+        }
+        else if (other.gameObject.tag == "slowing_trap")
+        {
+            statusCounter = 0;
+            Debug.Log("slowing trigger detected!");
+            status = Status.slowed;
+            if(velocityX > slowedMaxVelocity) //slow down players velocity if they're running faster
+            {
+                velocityX = slowedMaxVelocity;
+            }
+        }
+        else if (other.gameObject.tag == "speed_up")
+        {
+            statusCounter = 0;
+            status = Status.speedy;
+            velocityX = speedyMaxVelocity;
+        }
+        else if (other.gameObject.tag == "finish")
+        {
+            Debug.Log("race time: " + raceTime);
+            status = Status.finished;
             velocityX = 0;
         }
     }
@@ -127,6 +183,14 @@ public class Player : MonoBehaviour {
                 jumping = true;
                 //Debug.Log("Y velocity: " + velocityY);
             }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "slowing_trap")
+        {
+            status = Status.normal;
         }
     }
 }

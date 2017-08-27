a483ef8ae0226bfb930118802de338e419988e66
Jamie Zhang <jmzhang003@gmail.com>
2017-08-26 20:32:30 -0700

    3 and 4 player split screen

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -36,5 +36,32 @@ public class PlayerController : MonoBehaviour {
             Camera p2Cam = player2.transform.Find("Main Camera").GetComponent<Camera>();
             p2Cam.rect = new Rect(0, 0.5f, 1, 0.5f);
         }
+        else if(gm.GetPlayerCount() == 3)
+        {
+            GameObject player1 = players[0];
+            GameObject player2 = players[1];
+            GameObject player3 = players[2];
+            Camera p1Cam = player1.transform.Find("Main Camera").GetComponent<Camera>();
+            p1Cam.rect = new Rect(0, 0, 1, 0.33f);
+            Camera p2Cam = player2.transform.Find("Main Camera").GetComponent<Camera>();
+            p2Cam.rect = new Rect(0, 0.33f, 1, 0.34f);
+            Camera p3Cam = player3.transform.Find("Main Camera").GetComponent<Camera>();
+            p3Cam.rect = new Rect(0, 0.67f, 1, 0.33f);
+        }
+        else if (gm.GetPlayerCount() == 4)
+        {
+            GameObject player1 = players[0];
+            GameObject player2 = players[1];
+            GameObject player3 = players[2];
+            GameObject player4 = players[3];
+            Camera p1Cam = player1.transform.Find("Main Camera").GetComponent<Camera>();
+            p1Cam.rect = new Rect(0, 0, 0.5f, 0.5f);
+            Camera p2Cam = player2.transform.Find("Main Camera").GetComponent<Camera>();
+            p2Cam.rect = new Rect(0.5f, 0, 0.5f, 0.5f);
+            Camera p3Cam = player3.transform.Find("Main Camera").GetComponent<Camera>();
+            p3Cam.rect = new Rect(0, 0.5f, 0.5f, 0.5f);
+            Camera p4Cam = player4.transform.Find("Main Camera").GetComponent<Camera>();
+            p4Cam.rect = new Rect(0.5f, 0.5f, 0.5f, 0.5f);
+        }
     }
 }

8b89ad939d3ac49973b3a1dff0cb2ab6e24de43d
Kalan Miurrelle <kalan.miurrelle@gmail.com>
2017-08-27 18:12:30 -0700

    typing terror

--- Assets/Scripts/TrapManager.cs
+++ Assets/Scripts/TrapManager.cs
@@ -23,7 +23,10 @@ public class TrapManager : MonoBehaviour
     public Sprite buttonTrapSprite;
 
     public GameObject coinTrap;
-    public Sprite coninTrapSprite;
+    public Sprite coinTrapSprite;
+
+    public GameObject badCoinTrap;
+    public Sprite badCoinTrapSprite;
 
     public GameObject knifeTrap;
     public Sprite knifeTrapSprite;
@@ -43,8 +46,11 @@ public class TrapManager : MonoBehaviour
     public GameObject magnetTrap;
     public Sprite magnetTrapSprite;
 
-    public GameObject stopWatchTrap;
-    public Sprite stopWatchTrapSprite;
+    public GameObject badMagnetTrap;
+    public Sprite badMagnetTrapSprite;
+
+    public GameObject stopwatchTrap;
+    public Sprite stopwatchTrapSprite;
 
     public GameObject ufoTrap;
     public Sprite ufoTrapSprite;
@@ -125,7 +131,18 @@ UFO
         possibleTrapList.Add(new PlaceableObject("Poison Toast", "Slows down any player who steps on this trap for 3 seconds.", bearTrap, bearTrapSprite));
         possibleTrapList.Add(new PlaceableObject("Box of Spaghetti", "This is a box of spaghetti. It says Bring 4 - 6 quarts of water to a rolling boil, add salt to taste.Add contents of the package to boiling water.Return to a boil. For authentic al dente pasta, boil uncovered, stirring occasionally for 9 minutes. ... Remove from heat. Serve immediately with your favorite Barilla sauce.", spaghettiTrap, spaghettiTrapSprite));
         possibleTrapList.Add(new PlaceableObject("Button", "Will you push the big red button?", buttonTrap, buttonTrapSprite));
-        possibleTrapList.Add(new PlaceableObject("Button", "Will you push the big red button?", buttonTrap, buttonTrapSprite));
+        possibleTrapList.Add(new PlaceableObject("Coin", "Everything for a price", coinTrap, coinTrapSprite));
+        possibleTrapList.Add(new PlaceableObject("Coin", "Everything for a price", badCoinTrap, badCoinTrapSprite));
+        possibleTrapList.Add(new PlaceableObject("Knife of Greater Yandere Prowess", "The best naifu of you have ever seen", knifeTrap, knifeTrapSprite));
+        possibleTrapList.Add(new PlaceableObject("Death Note", "Light did nothing wrong", deathNoteTrap, deathNoteTrapSprite));
+        possibleTrapList.Add(new PlaceableObject("Glasses", "Look with your special eyes", glassesTrap, glassesTrapSprite));
+        possibleTrapList.Add(new PlaceableObject("Throwing Star", "We had a ninja but we ran out of time", starTrap, starTrapSprite));
+        possibleTrapList.Add(new PlaceableObject("Pillow", "Pillow fight!", pillowTrap, pillowTrapSprite));
+        possibleTrapList.Add(new PlaceableObject("Senpai Magnet", "Surely I can get closer to senpai with this", magnetTrap, magnetTrapSprite));
+        possibleTrapList.Add(new PlaceableObject("Senpai Magnet", "Surely I can get closer to senpai with this", badMagnetTrap, badMagnetTrapSprite));
+        possibleTrapList.Add(new PlaceableObject("Stopwatch", "ZA WARUDO!!!!", stopwatchTrap, stopwatchTrapSprite));
+        possibleTrapList.Add(new PlaceableObject("UFO", "The truth is out there", ufoTrap, ufoTrapSprite));
+
 
     }
 
